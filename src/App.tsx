391c8d4427e4173c51231cf28dcf9d8ebde58e4a
taowei <1536452459@qq.com>
2025-09-25 14:51:28 +0800

    feat: tsx语法

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const fn = (param: number) => console.log("click", param);
+  const b1 = false;
+  const id = "999";
+  const style = { color: "red" };
+  const cls1 = "a";
+  const cls2 = "b";
+  const html = '<h1 style="color:red">title</h1>';
+  const arr = [1, 2, 3, 4, 5];
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div>{"1"}</div>
+      <div>{1}</div>
+      <div>{new Date().getTime()}</div>
+      <div>{[1, 2, 3]}</div>
+      <div>{b1 ? 1 : 2}</div>
+      <button onClick={() => fn(123)}>click</button>
+      <div id={id} style={style} className={cls1}>
+        test
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={`${cls1} ${cls2} aa bb cc`}>多个class</div>
+      <div dangerouslySetInnerHTML={{ __html: html }}></div>
+
+      {arr.map((item) => {
+        return <div id={String(item)}>{item}</div>;
+      })}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
